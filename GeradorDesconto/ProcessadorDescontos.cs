8f86b71b81c9f50a4ca1dababd220d77fae7eef6
Irenita-Lopes <irenita.f.lopes@gmail.com>
2026-01-27 21:36:04 -0300

    Separando as responsabilidades do processamento

---
+++ GeradorDesconto/ProcessadorDescontos.cs
@@ -0,0 +1,39 @@
+﻿namespace GeradorDesconto
+{
+    internal static class ProcessadorDescontos
+    {
+        public static void Executar()
+        {
+            ExibirMensagem("Gerador de Descontos");
+            
+            ExibirMensagem("Digite o valor original do produto: ");
+            decimal valorOriginal = LerValor();
+            
+            ExibirMensagem("Digite a porcentagem de desconto (ex: 10 para 10%): ");
+   
+            decimal porcentagemDesconto = LerValor();
+            
+            decimal valorDesconto = CalcularDesconto(valorOriginal, porcentagemDesconto);
+            
+            decimal valorFinal = valorOriginal - valorDesconto;
+            
+            ExibirMensagem($"Valor do Desconto: R$ {valorDesconto:F2}");
+            ExibirMensagem($"Valor Final após Desconto: R$ {valorFinal:F2}");
+        }
+       
+        private static decimal LerValor()
+        {
+            return Convert.ToDecimal(Console.ReadLine());
+        }
+
+        private static void ExibirMensagem(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+
+        private static decimal CalcularDesconto(decimal valorOriginal, decimal porcentagemDesconto)
+        {
+            return (valorOriginal * porcentagemDesconto) / 100;
+        }
+    }
+}
